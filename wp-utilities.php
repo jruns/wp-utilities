98a8acb335cde4016214189eb1537900d42a213d
Jason Schramm <jason.schramm@gmail.com>
2025-07-28 12:27:06 -0400

    Update author url

--- wp-utilities.php
+++ wp-utilities.php
@@ -16,7 +16,7 @@
  * Description:       Utilities to improve the performance of your WordPress site.
  * Version:           0.1.0
  * Author:            Jason Schramm
- * Author URI:        https://jruns.github.io//
+ * Author URI:        https://jruns.github.io/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wp-utilities
