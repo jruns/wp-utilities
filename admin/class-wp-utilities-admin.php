46b636ce895adc1e3688d57e7968f13216145c64
Jason Schramm <jason.schramm@gmail.com>
2025-08-05 11:04:32 -0400

    Strip out unneeded plugin code

--- admin/class-wp-utilities-admin.php
+++ admin/class-wp-utilities-admin.php
@@ -13,9 +13,6 @@
 /**
  * The admin-specific functionality of the plugin.
  *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
- *
  * @package    Wp_Utilities
  * @subpackage Wp_Utilities/admin
  * @author     Jason Schramm <jason.runs@proton.me>
@@ -53,52 +50,6 @@ class Wp_Utilities_Admin {
 		$this->version = $version;
 	}
 
-	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    0.1.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp_Utilities_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp_Utilities_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-utilities-admin.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    0.1.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp_Utilities_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp_Utilities_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-utilities-admin.js', array( 'jquery' ), $this->version, false );
-
-	}
-
 	public function add_options_page() {
 		add_options_page(
 			'WP Performance Utilities',
@@ -108,7 +59,6 @@ class Wp_Utilities_Admin {
 			array( $this, 'render_options_page' )
 		);
 	}
-
 	
     public function registersettings() {
         register_setting( 'wp-utilities', 'wp_utilities_disable_jquery_migrate');
