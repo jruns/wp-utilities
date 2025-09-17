bc8c978acac2d1701e88920ee0ec5eac0eed1bb2
Jason Schramm <jason.schramm@gmail.com>
2025-09-17 15:48:46 -0400

    Move script tag attributes that exclude a tag from being delayed into an array for easier modification

--- includes/class-wp-utilities-html-buffer.php
+++ includes/class-wp-utilities-html-buffer.php
@@ -68,7 +68,8 @@ class Wp_Utilities_Html_Buffer {
 
 					if ( ! empty( $match_strings[ $type ] ) && preg_match( $regex_string, $tag_contents ) ) {
 						if ( $operation === 'delay' ) {
-							if ( 1 !== preg_match( '/<script[^>]*nodelay|nowprocket|data-pagespeed-no-defer[^>]*>/im', $tag_contents ) ) {
+							$exclusion_attributes = array( 'nodelay', 'nowprocket', 'data-pagespeed-no-defer' );
+							if ( 1 !== preg_match( '/<script[^>]*' . join( '|', $exclusion_attributes ) . '[^>]*>/im', $tag_contents ) ) {
 								if ( 0 === preg_match( '/<script[^>]* defer[^>]*>/im', $tag_contents ) ) {
 									$tag_contents = str_replace( '<script', '<script defer', $tag_contents );
 								}
