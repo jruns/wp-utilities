935ffa194a65acdde7c410e1d054e1f8f0b70b8c
Jason Schramm <jason.schramm@gmail.com>
2025-09-19 22:37:28 -0400

    Rename user interaction delay script source

---
+++ src/js/user_interaction_delay_script.js
@@ -0,0 +1,20 @@
+{
+    const autoLoadDelay = wputilAutoLoadDelay;
+	const eventList = ["mouseover","keydown","touchmove","touchstart"];
+    const load = () => {
+		const event = new Event("DOMUserInteraction");
+		document.dispatchEvent(event);
+
+		console.log("interacted");
+
+		document.querySelectorAll("script[data-type=lazy]").forEach(el => el.src = el.dataset.src);
+
+		eventList.forEach(e => window.removeEventListener(e, trigger, {passive: true, once: true}));
+	}
+    const timer = setTimeout(load, autoLoadDelay);
+    const trigger = () => {
+        load();
+        clearTimeout(timer);
+    };
+    eventList.forEach(e => window.addEventListener(e, trigger, {passive: true, once: true}));
+}
